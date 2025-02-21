21c768e3561009dd342f0001f3b1263b48656ecf
Emanuele Gissi <emanuele.gissi@gmail.com>
2025-02-21 15:16:26 +0100

    Port widget to Vue 3

--- flowbuttons/modules.js
+++ flowbuttons/modules.js
@@ -1,21 +1,21 @@
 "use strict"; // strict mode
 
-const requestKeys =  ["Request_date", "Requested_by", "Request", "Process", "Status"];
+const requestedCols =  ["Request_date", "Requested_by", "Request", "Process", "Status"];
 
 // Duplicate request
 
-export async function duplicateRequest(context, action, record) {
+export async function duplicateRequest(action, record) {
   return await duplicateRecordWrap({
     tableId: "Requests",
     record: record,
-    keys: requestKeys,
+    cols: requestedCols,
     confirmText: `<${action.label}> confirmed?`,
   });
 }
 
 // Update request status, but only for selected tools
 
-export async function updateRequestStatusOnlySel(context, action, record) {
+export async function updateRequestStatusOnlySel(action, record) {
   if (record.Unsel_goods_ids.length > 0) {  // are there unselected tools?
     // Ask for confirmation
     const confirmText = `Execute <${action.label}> on selected records only?`;
@@ -24,7 +24,7 @@ export async function updateRequestStatusOnlySel(context, action, record) {
     const newId = await duplicateRecordWrap({
       tableId: "Requests",
       record: record,
-      keys: requestKeys,
+      cols: requestedCols,
       confirmText: null,
       setCursor: false,
     });
@@ -32,10 +32,10 @@ export async function updateRequestStatusOnlySel(context, action, record) {
     await updateRecordsWrap({
       tableId: "Goods",  // children table to split
       ids: record.Unsel_goods_ids,   // list of unselected children ids in parent table
-      fs: {"Request": newId, "Select": true},
+      fields: {"Request": newId, "Select": true},
       confirmText: null,
     });
   }
   // Update original request status
-  await updateStatus(context, action, record);
+  await updateStatus(action, record);
 }
